21d2c9c29274ee9b284bd254c489acb728208e0b
ayoubass54 <ayoubelaz542@gmail.com>
2026-09-18 01:56:10 +0200

    comparaisons numerique/analytique

---
+++ benchmarks/analytical_vs_numerical/delta.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+#include "../../include/option.hpp"
+#include "../../include/greeks.hpp"
+#include "../../include/black_sholes.hpp"
+
+
+
+int main() {
+    parameters par1;
+    par1.S0 = 100;
+    par1.K = 100;
+    par1.T = 1;
+    par1.r = 0.05;
+    par1.sigma = 0.20;
+
+    Option C1 = Option(par1, typeoption::CALL);
+    double deltaAnalytic = Greeks::deltaC(C1);
+
+    std::cout << std::fixed << std::setprecision(15);
+    std::cout << "Delta analytique: " << deltaAnalytic << std::endl;
+    std::cout << "Deltas numeriques: " << std::endl;
+
+    double h;;
+    double deltaNumeric;
+    parameters par2 = par1;
+    double V1, V2;
+
+    for (int i = 1; i < 15; i++) {
+        h = pow(10, -i);
+        par1.S0 = 100 + h;
+        par2.S0 = 100 - h;
+        V1 = BlackSholes::price(Option(par1, typeoption::CALL));
+        V2 = BlackSholes::price(Option(par2, typeoption::CALL));
+
+        deltaNumeric = (V1 - V2) / (2 * h);
+
+        std::cout << "h = " << h << ", delta = " <<  deltaNumeric
+        << ", l'erreur = " << deltaNumeric - deltaAnalytic
+        << std::endl;
+    }
+}
---
+++ benchmarks/analytical_vs_numerical/gamma.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+#include "../../include/option.hpp"
+#include "../../include/greeks.hpp"
+#include "../../include/black_sholes.hpp"
+
+
+
+int main() {
+    parameters par1;
+    par1.S0 = 100;
+    par1.K = 100;
+    par1.T = 1;
+    par1.r = 0.05;
+    par1.sigma = 0.20;
+
+    Option C1 = Option(par1, typeoption::CALL);
+    double gammaAnalytic = Greeks::gamma(C1);
+
+    std::cout << std::fixed << std::setprecision(15);
+    std::cout << "Gammas analytique: " << gammaAnalytic << std::endl;
+    std::cout << "Gammas numeriques: " << std::endl;
+
+    double V = BlackSholes::price(C1);
+    double h;
+    double gammaNumeric;
+    parameters par2 = par1;
+    double V1, V2;
+
+    for (int i = 1; i < 15; i++) {
+        h = pow(10, -i);
+        par1.S0 = 100 + h;
+        par2.S0 = 100 - h;
+        V1 = BlackSholes::price(Option(par1, typeoption::CALL));
+        V2 = BlackSholes::price(Option(par2, typeoption::CALL));
+
+        gammaNumeric = (V1 - 2*V + V2) / (h * h);
+
+        std::cout << "h = " << h << ", delta = " <<  gammaNumeric
+        << ", l'erreur = " << gammaNumeric - gammaAnalytic
+        << std::endl;
+    }
+}
