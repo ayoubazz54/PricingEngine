717d6b74ecbcde028a9383b441ffdb66a22be1b4
ayoubass54 <ayoubelaz542@gmail.com>
2026-09-17 23:27:28 +0200

    modified:   README.md 	new file:   build/CMakeCache.txt 	new file:   build/CMakeFiles/4.2.3/CMakeCCompiler.cmake 	new file:   build/CMakeFiles/4.2.3/CMakeCXXCompiler.cmake 	new file:   build/CMakeFiles/4.2.3/CMakeDetermineCompilerABI_C.bin 	new file:   build/CMakeFiles/4.2.3/CMakeDetermineCompilerABI_CXX.bin 	new file:   build/CMakeFiles/4.2.3/CMakeSystem.cmake 	new file:   build/CMakeFiles/4.2.3/CompilerIdC/CMakeCCompilerId.c 	new file:   build/CMakeFiles/4.2.3/CompilerIdC/a.out 	new file:   build/CMakeFiles/4.2.3/CompilerIdCXX/CMakeCXXCompilerId.cpp 	new file:   build/CMakeFiles/4.2.3/CompilerIdCXX/a.out 	new file:   build/CMakeFiles/CMakeConfigureLog.yaml 	new file:   build/CMakeFiles/CMakeDirectoryInformation.cmake 	new file:   build/CMakeFiles/InstallScripts.json 	new file:   build/CMakeFiles/Makefile.cmake 	new file:   build/CMakeFiles/Makefile2 	new file:   build/CMakeFiles/TargetDirectories.txt 	new file:   build/CMakeFiles/cmake.check_cache 	new file:   build/CMakeFiles/progress.marks 	new file:   build/Makefile 	new file:   build/cmake_install.cmake 	modified:   src/main.cpp 	modified:   src/pricer

---
+++ README.md
@@ -0,0 +1,6 @@
+# C++ Quantitative Pricing Engine
+
+
+## Current Usage
+
+Pricing Call and Put options using Blacksholes formulas.
--- src/main.cpp
+++ src/main.cpp
@@ -5,22 +5,26 @@
 int main() {
     parameters par;
     
-    std::cin >> par.S0;
+    par.S0 = 100;
     par.K = 100;
-    par.T = 1;
+    // par.T = 1;
     par.r = 0.05;
     par.sigma = 0.20;
 
-    Option C = Option(par, typeoption::CALL);
-    Option P = Option(par, typeoption::PUT);
+    while (true) {
+        std::cin >> par.T;
+        Option C = Option(par, typeoption::CALL);
+        Option P = Option(par, typeoption::PUT);
 
-    std::cout << "Le prix de l'option call est: "
-    << BlackSholes::price(C) << ". " << std::endl;
+        std::cout << "Le prix de l'option call est: "
+        << BlackSholes::price(C) << std::endl;
 
-    std::cout << "Le prix de l'option put est: "
-    << BlackSholes::price(P) << ". " << std::endl;
+        std::cout << "Le prix de l'option put est: "
+        << BlackSholes::price(P) << std::endl;
 
-    std::cout << "L'erreur du calcul: "
-    << BlackSholes::errorCP(C, BlackSholes::price(C), BlackSholes::price(P)) << std::endl;
+        std::cout << "L'erreur du calcul: "
+        << BlackSholes::errorCP(C, BlackSholes::price(C), BlackSholes::price(P)) << std::endl;
 
+    }
+    
 }
