a13ba2556d2fcd33d2eee3d14fe090ba94b9028f
ayoubass54 <ayoubelaz542@gmail.com>
2026-09-21 15:23:06 +0200

    Studying Parallelized Monte Carlo's convergence, and comparing it with previous models

--- plots/convergence.py
+++ plots/convergence.py
@@ -2,17 +2,21 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 df_MC = pd.read_csv("../benchmarks/monte_carlo/monte_carlo_convergence.csv")
-df_MC1234 = pd.read_csv("../benchmarks/monte_carlo/monte_carlo_convergence_1234.csv")
+df_MCP = pd.read_csv("../benchmarks/parallel/monte_carlo_convergence_parallel.csv")
+# df_MC1234 = pd.read_csv("../benchmarks/monte_carlo/monte_carlo_convergence_1234.csv")
 df_CRR = pd.read_csv("../benchmarks/crr_convergence/crr_convergence.csv")
 
 # For Monte Carlo (42):
 fig, ax = plt.subplots(figsize=(8, 6))
-ax.loglog(df_MC["M"], df_MC["standard_error"], marker="o", linestyle="-", label="Erreur Standard1")
-ax.loglog(df_MC["M"], df_MC["error"], marker="s", linestyle="--", label="Erreur Absolue (Monte Carlo vs Black-Scholes)1")
+ax.loglog(df_MC["M"], df_MC["standard_error"], marker="o", linestyle="-", label="Erreur Standard")
+ax.loglog(df_MC["M"], df_MC["error"], marker="s", linestyle="--", label="Erreur Absolue (Monte Carlo vs Black-Scholes)")
+
+ax.loglog(df_MCP["M"], df_MCP["standard_error"], marker="o", linestyle="-", label="Erreur Standard parallel")
+ax.loglog(df_MCP["M"], df_MCP["error"], marker="s", linestyle="--", label="Erreur Absolue (Monte Carlo parallelized vs Black-Scholes")
 
 # For Monte Carlo (1234):
-ax.loglog(df_MC1234["M"], df_MC1234["standard_error"], marker="o", linestyle="-", label="Erreur Standard2")
-ax.loglog(df_MC1234["M"], df_MC1234["error"], marker="s", linestyle="--", label="Erreur Absolue (Monte Carlo vs Black-Scholes)2")
+# ax.loglog(df_MC1234["M"], df_MC1234["standard_error"], marker="o", linestyle="-", label="Erreur Standard2")
+# ax.loglog(df_MC1234["M"], df_MC1234["error"], marker="s", linestyle="--", label="Erreur Absolue (Monte Carlo vs Black-Scholes)2")
 
 ax.set_xlabel("Nombre de simulations (M)")
 ax.set_ylabel("Erreur")
@@ -32,7 +36,6 @@ fig, ax = plt.subplots(figsize=(8, 6))
 ax.loglog(df_CRR["n"], df_CRR["error"], marker="s", linestyle="--", label="Erreur Absolue (CRR vs Black-Scholes)")
 
 ax.set_xlabel("Nombre de pas (n)")
-ax.set_xlabel("Nombre de pas (n)")
 ax.set_ylabel("Erreur")
 ax.set_title("Convergence de la méthode de Cox-Ross-Rubinstein")
 ax.legend()
@@ -46,8 +49,8 @@ print("Graphique sauvegardé avec succès sous : crr_convergence.png")
 fig, ax = plt.subplots(figsize=(8, 6))
 
 ax.loglog(df_CRR["time"], df_CRR["error"], marker="s", linestyle="--", label="Erreur Absolue (CRR vs Black-Scholes)")
-ax.loglog(df_MC["time"], df_MC["error"], marker="s", linestyle="--", label="Erreur Absolue (MC vs Black-Scholes)1")
-ax.loglog(df_MC1234["time"], df_MC1234["error"], marker="s", linestyle="--", label="Erreur Absolue (MC vs Black-Scholes)2")
+ax.loglog(df_MC["time"], df_MC["error"], marker="s", linestyle="--", label="Erreur Absolue (MC vs Black-Scholes)")
+ax.loglog(df_MCP["time"], df_MCP["error"], marker="s", linestyle="--", label="Erreur Absolue (MC paralelizd vs Black-Scholes)")
 
 ax.set_xlabel("temps consommé (par second)")
 ax.set_ylabel("Erreur")
