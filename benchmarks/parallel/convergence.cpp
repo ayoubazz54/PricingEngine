10f9b83a9760f469e9c2a3ee1cddb44a46f5ae2f
ayoubass54 <ayoubelaz542@gmail.com>
2026-09-21 19:02:56 +0200

    Studying Parallel Monte Carlo's convergence, for multiple numbers of threads

--- benchmarks/parallel/convergence.cpp
+++ benchmarks/parallel/convergence.cpp
@@ -14,6 +14,8 @@
 
 #include <vector>
 #include <cmath>
+#include <string>
+#include <fstream>
 
 
 int main() {
@@ -22,50 +24,54 @@ int main() {
 
     long double exactPrice = BlackSholes::price(option);
 
-    std::vector<int> simulations = { 1000, 10000, 100000, 1000000, 10000000, 100000000, 1000000000};
-    std::ofstream file("monte_carlo_convergence_parallel.csv");
+    std::vector<int> simulations = { 1000, 10000, 100000, 1000000, 10000000, 100000000};
+    std::vector<int> numThreads = { 1, 2, 4, 8, 12 };
+    for (int i : numThreads) {
+        std::ofstream file("monte_carlo_convergence_parallel_" + std::to_string(i) + ".csv");
 
-    if (!file.is_open()) {
-        std::cerr << "Error during CSV file opening\n";
-        return 1;
+        if (!file.is_open()) {
+            std::cerr << "Error during CSV file opening\n";
+            return 1;
+        }
+
+        file << "thread,M,price,standard_error,error,ci_low,ci_high,time\n";
+
+        const uint64_t seed = 42;
+
+        std::cout << std::fixed << std::setprecision(10);
+
+        for (int M : simulations) {
+
+            auto start = std::chrono::high_resolution_clock::now();
+            MonteCarloResult result = MonteCarloParallel::price(option, M, seed, 4);
+            auto end = std::chrono::high_resolution_clock::now();
+
+            std::chrono::duration<double> elapsed = end - start;
+
+            long double error = fabs(result.price - exactPrice);
+
+            file << i << "," 
+                << M << ","
+                << result.price << ","
+                << result.standardError << "," 
+                << error << ","
+                << result.confidenceLow << ","
+                << result.confidenceHigh << ","
+                << elapsed.count() << "\n";
+
+            std::cout 
+                << "M = " << M
+                << " Price = " << result.price 
+                << " SE = " << result.standardError 
+                << " Error = " << error 
+                << " Time = " << elapsed.count() << "s\n";
+
+        }
+
+        file.close();
+
+        std::cout << "\nCSV generated: monte_carlo_convergence_" + std::to_string(i) + ".csv\n";
     }
-
-    file << "M,price,standard_error,error,ci_low,ci_high,time\n";
-
-    const uint64_t seed = 42;
-
-    std::cout << std::fixed << std::setprecision(10);
-
-    for (int M : simulations) {
-
-        auto start = std::chrono::high_resolution_clock::now();
-        MonteCarloResult result = MonteCarloParallel::price(option, M, seed, 4);
-        auto end = std::chrono::high_resolution_clock::now();
-
-        std::chrono::duration<double> elapsed = end - start;
-
-        long double error = fabs(result.price - exactPrice);
-
-        file << M << ","
-             << result.price << ","
-             << result.standardError << "," 
-             << error << ","
-             << result.confidenceLow << ","
-             << result.confidenceHigh << ","
-             << elapsed.count() << "\n";
-
-        std::cout 
-            << "M = " << M
-            << " Price = " << result.price 
-            << " SE = " << result.standardError 
-            << " Error = " << error 
-            << " Time = " << elapsed.count() << "s\n";
-
-    }
-
-    file.close();
-
-    std::cout << "\nCSV generated: monte_carlo_convergence.csv\n";
 
     return 0;
 
---
+++ plots/parallel.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv("../benchmarks/monte_carlo/monte_carlo_convergence.csv")
+df_1 = pd.read_csv("../benchmarks/parallel/monte_carlo_convergence_parallel_1.csv")
+df_2 = pd.read_csv("../benchmarks/parallel/monte_carlo_convergence_parallel_2.csv")
+df_4 = pd.read_csv("../benchmarks/parallel/monte_carlo_convergence_parallel_4.csv")
+df_8 = pd.read_csv("../benchmarks/parallel/monte_carlo_convergence_parallel_8.csv")
+df_12 = pd.read_csv("../benchmarks/parallel/monte_carlo_convergence_parallel_12.csv")
+
+fig, ax = plt.subplots(figsize=(8, 6))
+
+ax.loglog(df["time"], df["error"], marker="s", linestyle="--", label="without parallelization")
+ax.loglog(df_1["time"], df_1["error"], marker="s", linestyle="--", label="1 thread")
+ax.loglog(df_2["time"], df_2["error"], marker="s", linestyle="--", label="2 threads")
+ax.loglog(df_4["time"], df_4["error"], marker="s", linestyle="--", label="4 threads")
+ax.loglog(df_8["time"], df_8["error"], marker="s", linestyle="--", label="8 threads")
+ax.loglog(df_12["time"], df_12["error"], marker="s", linestyle="--", label="12 threads")
+
+
+ax.set_xlabel("temps (s)")
+ax.set_ylabel("Erreur (Monte carlo - BlackScholes)")
+ax.set_title("Convergence de la méthode de Monte-Carlo")
+ax.legend()
+ax.grid(True, which="both", ls="--")
+
+plt.tight_layout()
+plt.savefig("monte_carlo_convergence_parallel.png", dpi=300)
+print("Graphique sauvegardé avec succès sous : monte_carlo_convergence_parallel.png")
